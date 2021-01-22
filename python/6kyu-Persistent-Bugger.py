9d9a48cd0ced41f23e1d59f7da0a7837c7859dee
Ekaterina Martynko <ekaterina.martynko@gmail.com>
2021-01-22 14:58:30 +0300

    [EDIT] Added docstring with examples

--- python/6kyu-Persistent-Bugger.py
+++ python/6kyu-Persistent-Bugger.py
@@ -1,4 +1,15 @@
 def persistence(n):
+    '''
+    Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, 
+    which is the number of times you must multiply the digits in num until you reach a single digit.
+    
+    >>> persistence(39)
+    3
+    >>> persistence(999)
+    4
+    >>> persistence(4)
+    0
+    '''
     if n < 10:
         return 0
     mult = 1
