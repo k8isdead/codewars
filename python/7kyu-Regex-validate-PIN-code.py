6b1a79c6f0f2b3b1db4433d5d76501d9d748ffe5
Ekaterina Martynko <ekaterina.martynko@gmail.com>
2021-01-22 14:55:20 +0300

    [EDIT] Added docstring and examples

--- python/7kyu-Regex-validate-PIN-code.py
+++ python/7kyu-Regex-validate-PIN-code.py
@@ -1,6 +1,19 @@
 import re
 
 def validate_pin(pin):
+    '''
+    ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+    If the function is passed a valid PIN string, return true, else return false.
+
+    >>> validate_pin("1234")
+    True
+    >>> validate_pin("12345")
+    False
+    >>> validate_pin("a234")
+    False
+
+    '''
     if re.fullmatch("\d{4}|\d{6}", pin):
         return True
     return False
